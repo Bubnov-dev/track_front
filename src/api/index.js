c37fa2ba101ed4041fe3a0e3db18bbb6f688a539
life-f <58433392+life-f@users.noreply.github.com>
2023-02-22 14:40:30 +0300

    fix validation and add password type

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 import {mapGetters, mapActions} from "vuex";
 
-// axios.defaults.baseURL = 'https://nowtime-back.ffox.site/api';
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
+axios.defaults.baseURL = 'https://nowtime-back.ffox.site/api';
+// axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
 // axios.defaults.headers.common['Authorization'] = 'Bearer 1|VeZIwdWDAeGQehhQQKPGzTv7fNmTVxzIoCsmKOry';
 axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded';
 
